29cfa466712ce708d50a617ca11d44041b5233f3
Ericles-Miller <59926475+Ericles-Miller@users.noreply.github.com>
2024-01-16 11:47:23 -0300

    intall package e create connection with database

---
+++ src/models/Users/infra/Entities/Users.cs
@@ -0,0 +1,11 @@
+namespace src.Models.Users.infra.Entities
+{
+  public class Users {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public DateTime CreatedAt { get; set; }
+  }
+}
+
