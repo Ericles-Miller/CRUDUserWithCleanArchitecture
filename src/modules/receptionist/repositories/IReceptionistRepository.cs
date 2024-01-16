bb586fae0862d1ba609cf7a816c5563cc3501c1c
Ericles-Miller <ericlesmiller15@gmail.com>
2024-01-16 09:40:59 -0300

    first commit

--- src/modules/receptionist/repositories/IReceptionistRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace ReceptionistNameSpace {
-    
-    public interface IReceptionistRepository {
-        List<Receptionist> createReceptionist(string address, string name,int number);
-        bool findByNumber(int number);
-
-        List<Receptionist> listReceptionist();
-    }
-}
