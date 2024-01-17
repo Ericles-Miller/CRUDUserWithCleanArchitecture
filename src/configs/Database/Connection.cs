b3ca6b5297648050f86075c4ab39c57656681930
Ericles-Miller <59926475+Ericles-Miller@users.noreply.github.com>
2024-01-17 17:42:40 -0300

    feat(users): create controller to users

--- src/configs/Database/Connection.cs
+++ src/configs/Database/Connection.cs
@@ -4,8 +4,10 @@ using src.Models.Users.infra.Entities;
 namespace src.configs.DataBase
 {
   public class Connection : DbContext {
+    // import entity class
     public DbSet<Users> Users { get; set; }
-
+    
+    // connection string
     protected override void OnConfiguring(DbContextOptionsBuilder options) => 
       options.UseSqlite("DataSource=app.db;Cache=Shared");
     
